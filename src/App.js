cbbee98f3ec659370f6167fd4d58d094022b61c1
VeerasamyM <veerabca09@gmail.com>
2022-11-14 01:16:42 +0530

    products added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
 import Navigation from "./Navigation";
+import Product from "./Product";
+import Product2 from "./Product2";
 function App() {
   return (
     <div className="App">
       <Header />
       <Navigation />
       <Home />
+      <Product/>
+      <Product2/>
+      
     </div>
   );
 }
---
+++ src/Product.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import './Product.css'
+
+
+function Product(props) {
+  return (
+    <div className='product'>
+      
+      <div className='Product_in'>
+      <div>  <img className='veera' src="https://rukminim1.flixcart.com/image/200/200/printer/j/j/y/hp-laserjet-m1005-multifunction-original-imadxhzpeb9qbrfg.jpeg?q=70" /></div>
+       <div><p className='jg'>Printerd</p></div> 
+       <div>  <p className='gj'>From ₹3999 </p></div>
+       <div> <p className='vj'>HP</p></div>
+      </div>
+    
+      <div className='Product_in'>
+      <div>  <img className='veera' src="https://rukminim1.flixcart.com/image/200/200/k0plpjk0/remote-control-toy/9/g/k/4-function-remote-control-high-speed-big-racing-car-toy-funkey-original-imafkg33umd8dy93.jpeg?q=70" ></img></div>
+      <div>  <p className='jg'>Remote Control Toys</p></div>
+      <div>  <p className='gj'>Buy Now!</p> 
+      </div>
+      <div>  <p className='vj'>Updo 80%</p></div>
+      
+      </div>
+      <div className='Product_in'>
+        <div><img  className='veera' src="https://rukminim1.flixcart.com/image/200/200/jlph9jk0/cycle/h/f/k/skyper-26t-sskp26bk0001-16-hero-original-imaf8ru5xysfgtmx.jpeg?q=70" ></img></div>
+        <div><p className='jg'>Cricket, Football &amp; More</p></div>
+        <div><p className='gj'>Ceat, Nivia, Li-Ning...</p>
+      </div>
+       <div> <p className='vj'>Up to 70% Off</p></div>
+       
+      </div>
+      <div className='Product_in'>
+       <div> <img className='veera' src="https://rukminim1.flixcart.com/image/200/200/knyxqq80/dslr-camera/r/y/x/digital-camera-eos-m50-mark-ii-eos-m50-mark-ii-canon-original-imag2gzkexzqhyhu.jpeg?q=70" ></img>
+        </div><div><p className='jg'>Top Mirrorless Cameras</p></div>
+        <div><p className='gj'>Canon, Sony, Fujifilm...</p>
+      </div>
+        <div><p className='vj'>Shop Now!</p></div>
+       
+      </div>
+      <div className='Product_in'>
+        <div><img className='veera' src="https://rukminim1.flixcart.com/image/200/200/knyxqq80/dslr-camera/r/y/x/digital-camera-eos-m50-mark-ii-eos-m50-mark-ii-canon-original-imag2gzkexzqhyhu.jpeg?q=70" ></img>
+        </div><div><p className='jg'>Top Mirrorless Cameras</p></div>
+        <div><p className='gj'>Canon, Sony, Fujifilm...</p>
+      </div>
+        <div><p className='vj'>Shop Now!</p></div>
+        
+      </div>
+  </div>
+  
+  )
+}
+
+export default Product
